25c74cffad885884427a827bdbfa6c0bf3d30009
Perming1126 <perming@student.usm.my>
2023-11-12 00:57:32 +0800

    checkCurrentUser  for HomePage

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -25,45 +25,74 @@ import { UserHome } from "./_components/user-home";
 import { Unauthorized } from "./_components/unauthorized";
 import { Button } from "@/components/ui/button"
 import { GraduationCap, ScrollText, Rocket, Heart } from "lucide-react";
+import Register from "../(auth)/register/page";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 
 const Home = async () => {
-
   const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return redirect("/register");
+  }
+
+  const onSubmit = () => {
+    return redirect("/register");
+  }
 
   return (
     <div>
-      <div className="border-4 shadow-md  w-6/12 text-center my-4 mx-auto">
-        {currentUser
-          ? <UserHome currentUser={currentUser} />
-          : <Unauthorized />
-        }
-      </div>
-
-      <div>
-        <div className="text-center my-0 mx-auto">
-          <h2>Home Page </h2>
-          <p>Where Internships and Simplicity at a place</p>
-
-          <div className="mt-10 space-x-5 > * + *">
-            <Button className="w-48 h-12">
-              <GraduationCap className="mr-2 h-4 w-4" /> Internship Listing
-
-            </Button>
-            <Button className="w-48 h-12">
-              <Rocket className="mr-2 h-4 w-4" />Project Showcase</Button>
+      {currentUser ? (
+        <div>
+          <div className="border-4 shadow-md  w-6/12 text-center my-4 mx-auto">
+            {currentUser
+              ? <UserHome currentUser={currentUser} />
+              : <Unauthorized />
+            }
           </div>
-          <div className="mt-5 space-x-5 > * + *">
-            <Button className="w-48 h-12">
-              <ScrollText className="mr-2 h-4 w-4" />Resume Upload</Button>
-            <Button className="w-48 h-12">
-              <Heart className="mr-2 h-4 w-4" />Favorite</Button>
+
+          <div>
+            <div className="text-center my-10 mx-auto">
+              <h2>Home Page </h2>
+              <p>Where Internships and Simplicity at a place</p>
+
+              <div className="mt-10 space-x-5 > * + *">
+                <Button className="w-48 h-12">
+                  <GraduationCap className="mr-2 h-4 w-4" /> Internship Listing
+
+                </Button>
+                <Button className="w-48 h-12" variant="outline">
+                  <Rocket className="mr-2 h-4 w-4" />Project Showcase</Button>
+              </div>
+              <div className="mt-5 space-x-5 > * + *">
+                <Button className="w-48 h-12">
+                  <ScrollText className="mr-2 h-4 w-4" />Resume Upload</Button>
+                <Button className="w-48 h-12">
+                  <Heart className="mr-2 h-4 w-4" />Favorite</Button>
+              </div>
+            </div>
           </div>
+
+
         </div>
-      </div>
+      ) : (
+        <div>
+          <div>
+            <h1>Invalid Account!</h1>
+          </div>
+          <div>
+            <Button onSubmit={onSubmit}>Register Now</Button>
+          </div>
 
+        </div>
 
+      )
+
+      }
     </div>
+
+
   );
 }
 
