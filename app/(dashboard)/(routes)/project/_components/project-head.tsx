a5b99dfe715bf0bf0de58f9c05877aa94a22b4a7
YohanLau <yohanlau1234@gmail.com>
2023-12-01 10:14:13 +0800

    Done project head

---
+++ app/(dashboard)/(routes)/project/_components/project-head.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import { User } from "@prisma/client";
+import { HeartButton } from "./heart-button";
+
+interface ProjectHeadProps {
+  id: string;
+  imageSrc: string;
+  currentUser: User;
+}
+
+const ProjectHead = ({ id, imageSrc, currentUser }: ProjectHeadProps) => {
+  return (
+    <div className="w-full h-[60vh] overflow-hidden rounded-xl relative mt-10">
+      <Image
+        alt="Project Image"
+        src={imageSrc}
+        fill
+        className="object-cover w-full"
+      />
+      <div className="absolute top-5 right-5">
+        <HeartButton projectId={id} user={currentUser} />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectHead;
