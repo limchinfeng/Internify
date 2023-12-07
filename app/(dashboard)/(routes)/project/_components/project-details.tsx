f55a64a941f7ae972ee7546b6268e84eabc54d80
YohanLau <yohanlau1234@gmail.com>
2023-12-07 14:29:22 +0800

    Done project details

--- app/(dashboard)/(routes)/project/_components/project-details.tsx
+++ app/(dashboard)/(routes)/project/_components/project-details.tsx
@@ -3,50 +3,54 @@
 import Image from "next/image";
 import avatar from "@/public/images/placeholder.jpg";
 import Link from "next/link";
-import { User } from "@prisma/client";
 import { useOrigin } from "@/hooks/use-origin";
 import { BsTelephone } from "react-icons/bs";
-import { AiOutlineMail, AiOutlineLink } from "react-icons/ai";
+import { IoIosLink } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa6";
+import { CiMail } from "react-icons/ci";
 
 interface ProjectDetailsProps {
+  id: string;
   title: string;
   category: string;
   imageSrc: string;
   name: string;
   phone: string;
   email: string;
-  currentUser: User;
+  link: string;
 }
 
 const ProjectDetails = ({
+  id,
   title,
   category,
   name,
   imageSrc,
   phone,
   email,
-  currentUser,
+  link,
 }: ProjectDetailsProps) => {
   const origin = useOrigin();
+
+  var iconStyle = "m-1 h-5 w-5";
 
   return (
     <div className="flex flex-col gap-7">
       <div>
         <h1 className="text-black">Project Title</h1>
-        <p className="text-3xl mt-2">{title}</p>
+        <p className="text-2xl mt-2">{title}</p>
       </div>
 
       <div>
         <h1 className="text-black">Project Category</h1>
-        <p className="text-xl mt-2">{category}</p>
+        <p className="text-2xl mt-2">{category}</p>
       </div>
 
       <div>
         <h1 className="text-black">User Details</h1>
         <div className="flex flex-col gap-3 px-5 mt-2">
           <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-2 mt-2">
+            <div className="flex flex-row gap-3 mt-2">
               <Image
                 className="rounded-full"
                 height="30"
@@ -58,32 +62,30 @@ const ProjectDetails = ({
             </div>
           </div>
           <p className="text-xl font-sans flex gap-3">
-            <BsTelephone />
+            <BsTelephone className={iconStyle} />
             {phone}
           </p>
           <p className="text-xl font-sans flex gap-3">
-            <AiOutlineMail />
+            <CiMail className={iconStyle} />
             {email}
           </p>
           <p className="text-xl font-sans flex gap-3">
-            <FaLinkedin />
+            <FaLinkedin className={iconStyle} />
             <Link
-              href={`http://${currentUser.link}`}
+              href={`http://${link}`}
               target="_blank"
               className="text-lg font-medium italic text-primary hover:text-blue-800 transition hover:underline"
             >
               LinkedIn Profile
             </Link>
-            {!currentUser.link && (
-              <p className="font-medium italic text-lg">
-                No Link
-              </p>
+            {!link && (
+              <p className="font-medium italic text-lg font-sans">No Link</p>
             )}
           </p>
           <p className="text-xl font-sans flex gap-3">
-            <AiOutlineLink />
+            <IoIosLink className={iconStyle} />
             <Link
-              href={`${origin}/profile/${currentUser.id}`}
+              href={`${origin}/profile/${id}`}
               target="_blank"
               className="text-lg font-medium italic text-primary hover:text-blue-800 transition hover:underline"
             >
