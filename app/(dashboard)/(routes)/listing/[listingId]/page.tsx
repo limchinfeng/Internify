002abcf4a9bd5e5c8c9530c9b0bf60c665e4c546
supershuaifeng <limchinfeng@gmail.com>
2023-12-17 23:12:22 +0800

    Done individual listing image

--- app/(dashboard)/(routes)/listing/[listingId]/page.tsx
+++ app/(dashboard)/(routes)/listing/[listingId]/page.tsx
@@ -7,10 +7,58 @@
   3. shows the job listing & company details (can design what you like )
 */
 
-const ListingIdPage = () => {
+import getCurrentUser from "@/actions/getCurrentUser";
+import prismadb from "@/lib/prismadb";
+import { redirect } from "next/navigation";
+import { ListingIdHead } from "./_components/listing-id-head";
+
+const ListingIdPage =  async ({params} : {params: { listingId: string}}) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return redirect("/");
+  }
+
+  const listing = await prismadb.listing.findUnique({
+    where: {
+      id: params.listingId,
+    },
+    include: {
+      category: true,
+      user: true,
+    },
+  });
+
+  if (!listing) {
+    return redirect("/");
+  }
+  
   return (  
-    <div>
-      individual listing id page
+    <div className="p-6 w-full flex flex-col items-center justify-center gap-10">
+      <ListingIdHead 
+        id={listing.id}
+        imageUrl={listing.imageUrl!}
+        currentUser={currentUser} 
+      />
+
+      <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 md:gap-10">
+        <div>
+          <div className="flex flex-col items-center gap-x-4 gap-4">
+            {/* <ProfileName currentUser={currentUser} />
+            <ProfilePhone currentUser={currentUser} />
+            <ProfileEmail currentUser={currentUser} /> */}
+            dddd
+          </div>
+        </div>
+
+        <div className="mt-4 md:mt-0">
+          <div className="flex flex-col items-center gap-x-4 gap-4">
+            {/* <ProfileLink currentUser={currentUser} />
+            <ProfileDescription currentUser={currentUser} /> */}
+            ddd
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
