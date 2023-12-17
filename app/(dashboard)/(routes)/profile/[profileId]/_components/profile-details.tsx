6020ad1effdc6152475ad9051bf484d0f090e976
YohanLau <yohanlau1234@gmail.com>
2023-12-17 22:35:57 +0800

    Changed Public User Profile Layout

--- app/(dashboard)/(routes)/profile/[profileId]/_components/profile-details.tsx
+++ app/(dashboard)/(routes)/profile/[profileId]/_components/profile-details.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Preview } from "@/components/description-preview";
 import Link from "next/link";
 import { AiOutlineMail } from "react-icons/ai";
@@ -26,61 +24,65 @@ export const ProfileDetails = ({
   var iconStyle = "m-1 h-5 w-5";
 
   return (
-    <div className="flex flex-col gap-6">
-      <div className="border rounded-md p-4 w-full">
-        <div className="mx-6">
-          <div className="flex flex-row gap-3">
-            <LuUser2 className={iconStyle} />
-            <h1 className="text-lg text-black">Name</h1>
+    <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 md:gap-10">
+      <div className="flex flex-col items-center gap-x-4 gap-4">
+        <div className="border rounded-md p-4 w-full">
+          <div className="mx-6">
+            <div className="flex flex-row gap-3">
+              <LuUser2 className={iconStyle} />
+              <h1 className="text-lg text-black">Name</h1>
+            </div>
+            <p className="text-xl mx-11 font-sans mt-1">{name}</p>
           </div>
-          <p className="text-xl mx-11 font-sans mt-1">{name}</p>
+        </div>
+
+        <div className="border rounded-md p-4 w-full">
+          <div className="mx-6">
+            <div className="flex flex-row gap-3">
+              <BsTelephone className={iconStyle} />
+              <h1 className="text-lg text-black">Phone</h1>
+            </div>
+            <p className="text-xl mx-11 font-sans mt-1">{phone}</p>
+          </div>
+        </div>
+
+        <div className="border rounded-md p-4 w-full">
+          <div className="mx-6">
+            <div className="flex flex-row gap-3">
+              <AiOutlineMail className={iconStyle} />
+              <h1 className="text-lg text-black">Email</h1>
+            </div>
+            <p className="text-xl mx-11 font-sans mt-1">{email}</p>
+          </div>
         </div>
       </div>
 
-      <div className="border rounded-md p-4 w-full">
-        <div className="mx-6">
-          <div className="flex flex-row gap-3">
-            <BsTelephone className={iconStyle} />
-            <h1 className="text-lg text-black">Phone</h1>
+      <div className="flex flex-col items-center gap-x-4 gap-4 mt-4 md:mt-0">
+        <div className="border rounded-md p-4 w-full">
+          <div className="mx-6">
+            <div className="flex flex-row gap-3">
+              <IoIosLink className={iconStyle} />
+              <h1 className="text-lg text-black">Link</h1>
+            </div>
+            <Link
+              href={`http://${link}`}
+              target="_blank"
+              className="text-xl font-medium italic text-primary hover:text-blue-800 transition hover:underline mx-11 font-sans mt-1"
+            >
+              {link}
+            </Link>
+            {!link && <p className="font-medium italic text-xl">No Link</p>}
           </div>
-          <p className="text-xl mx-11 font-sans mt-1">{phone}</p>
         </div>
-      </div>
 
-      <div className="border rounded-md p-4 w-full">
-        <div className="mx-6">
-          <div className="flex flex-row gap-3">
-            <AiOutlineMail className={iconStyle} />
-            <h1 className="text-lg text-black">Email</h1>
+        <div className="border rounded-md p-4 w-full">
+          <div className="mx-6">
+            <div className="flex flex-row gap-3">
+              <TbFileDescription className={iconStyle} />
+              <h1 className="text-lg text-black">Description</h1>
+            </div>
+            {description && <Preview value={description} />}
           </div>
-          <p className="text-xl mx-11 font-sans mt-1">{email}</p>
-        </div>
-      </div>
-
-      <div className="border rounded-md p-4 w-full">
-        <div className="mx-6">
-          <div className="flex flex-row gap-3">
-            <IoIosLink className={iconStyle} />
-            <h1 className="text-lg text-black">Link</h1>
-          </div>
-          <Link
-            href={`http://${link}`}
-            target="_blank"
-            className="text-xl font-medium italic text-primary hover:text-blue-800 transition hover:underline mx-11 font-sans mt-1"
-          >
-            {link}
-          </Link>
-          {!link && <p className="font-medium italic text-xl">No Link</p>}
-        </div>
-      </div>
-
-      <div className="border rounded-md p-4 w-full">
-        <div className="mx-6">
-          <div className="flex flex-row gap-3">
-            <TbFileDescription className={iconStyle} />
-            <h1 className="text-lg text-black">Description</h1>
-          </div>
-          {description && <Preview value={description} />}
         </div>
       </div>
     </div>
