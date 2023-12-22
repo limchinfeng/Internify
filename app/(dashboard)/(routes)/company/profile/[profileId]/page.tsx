c525aa0bc20b32ee3bc3b3bfd483ab8643d03ef7
supershuaifeng <limchinfeng@gmail.com>
2023-12-22 17:17:43 +0800

    Done delete company profile page

--- app/(dashboard)/(routes)/company/profile/[profileId]/page.tsx
+++ app/(dashboard)/(routes)/company/profile/[profileId]/page.tsx
@@ -1,19 +1,11 @@
-/*
-  TASK - Angel
-  create a page for the public user to view the company page (like others view your social media profile)
+import { redirect } from "next/navigation";
 
-  1. get current user
-  2. check if current user is valid (can refer to others page)
-  3. shows the company details (can design what you like )
-  4. get the company user listings and shows it in table (can refer to company profile data table)
-*/
-
-const ProfileIdPage = () => {
-  return (  
-    <div>
-      company profile page public view
-    </div>
-  );
+const ProfileIdPage = ({
+  params,
+}: {
+  params: { profileId: string };
+}) => {
+  return redirect(`/profile/${params.profileId}`)
 }
  
 export default ProfileIdPage;
