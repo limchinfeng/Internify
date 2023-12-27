5752f8564e17412b2c8acbc3559edb83d3d6eb86
Perming1126 <perming@student.usm.my>
2023-12-27 23:21:07 +0800

    add error page

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+
+interface ErrorStateProps {
+    error: Error
+}
+
+const Error: React.FC<ErrorStateProps> = ({
+    error
+}) => {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className='h-full w-full flex flex-col items-center justify-center gap-3'>
+            <div>
+                Uh Oh
+            </div>
+            <div>
+                Something went wrong
+            </div>
+        </div>
+    )
+}
+
+export default Error
