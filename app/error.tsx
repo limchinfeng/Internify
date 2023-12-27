70484420649aed85a05795d5a89250b2721aaa50
Perming1126 <perming@student.usm.my>
2023-12-27 12:54:52 +0800

    bcrypt build next

--- app/error.tsx
+++ app/error.tsx
@@ -3,24 +3,24 @@
 import React, { useEffect } from 'react'
 
 interface ErrorStateProps {
-    error: Error
+  error: Error
 }
 
 const Error: React.FC<ErrorStateProps> = ({
-    error
+  error
 }) => {
-    useEffect(() => {
-        console.error(error);
-    }, [error]);
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
 
   return (
     <div className='h-full w-full flex flex-col items-center justify-center gap-3'>
-        <div>
-          Uh Oh'
-        </div>
-        <div>
-          Something went wrong'
-        </div>
+      <div>
+        Uh Oh
+      </div>
+      <div>
+        Something went wrong
+      </div>
     </div>
   )
 }
