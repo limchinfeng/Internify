4d7d8a48e6be05fffdef6ee262cf3426ab0a9da9
Perming1126 <perming@student.usm.my>
2023-12-27 21:42:00 +0800

    fix public profile page

--- app/(dashboard)/(routes)/profile/_components/profile-page-link.tsx
+++ app/(dashboard)/(routes)/profile/_components/profile-page-link.tsx
@@ -2,12 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useOrigin } from "@/hooks/use-origin";
 import { User } from "@prisma/client";
 import { useState } from "react";
 import toast from "react-hot-toast";
-import {AiOutlineCheckCircle} from "react-icons/ai"
-import {BiCopy} from "react-icons/bi";
+import { AiOutlineCheckCircle } from "react-icons/ai"
+import { BiCopy } from "react-icons/bi";
 
 interface ProfilePageLinkProps {
   currentUser: User;
@@ -18,9 +17,9 @@ export const ProfilePageLink = ({
 }: ProfilePageLinkProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [copied, setCopied] = useState(false);
-  const origin = useOrigin();
+  // const origin = useOrigin();
 
-  let userProfilePageLink = `${origin}/profile/${currentUser.id}`;
+  let userProfilePageLink = `https://internify-deploy.vercel.app/profile/${currentUser.id}`;
 
   // {currentUser.isCompany 
   //   ? userProfilePageLink = `${origin}/company/profile/${currentUser.id}`
@@ -38,35 +37,35 @@ export const ProfilePageLink = ({
       setIsLoading(false);
     }, 2000);
   }
-  
+
   return (
     <div className="flex flex-col md:w-4/5 w-[90%] ">
-        <h1 className="text-primary font-bold">
-          Profile Page Link
-        </h1>
-        <div className="flex items-center mt-2  border border-primary w-full rounded-lg">
-          <Input 
-            disabled={isLoading}
-            className="w-full p-2"
-            value={userProfilePageLink}
-            readOnly
-          />
-          <Button 
-            size="icon"
-            disabled={isLoading} 
-            onClick={onCopy} 
-            className="h-10 w-10 bg-primary text-white hover:bg-primary/90 flex  justify-center items-center"
-          >
-            {copied ?
-              <>
-                <AiOutlineCheckCircle />
-              </> : 
-              <>
-                <BiCopy />
-              </>
-            }
-          </Button>
-        </div>
+      <h1 className="text-primary font-bold">
+        Profile Page Link
+      </h1>
+      <div className="flex items-center mt-2  border border-primary w-full rounded-lg">
+        <Input
+          disabled={isLoading}
+          className="w-full p-2"
+          value={userProfilePageLink}
+          readOnly
+        />
+        <Button
+          size="icon"
+          disabled={isLoading}
+          onClick={onCopy}
+          className="h-10 w-10 bg-primary text-white hover:bg-primary/90 flex  justify-center items-center"
+        >
+          {copied ?
+            <>
+              <AiOutlineCheckCircle />
+            </> :
+            <>
+              <BiCopy />
+            </>
+          }
+        </Button>
       </div>
+    </div>
   )
 }
