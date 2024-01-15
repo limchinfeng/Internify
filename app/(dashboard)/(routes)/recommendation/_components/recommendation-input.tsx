3edc8f0fdc645b5bb235f3af68b724e36bb72409
supershuaifeng <limchinfeng@gmail.com>
2024-01-15 10:32:55 +0800

    Done loader

--- app/(dashboard)/(routes)/recommendation/_components/recommendation-input.tsx
+++ app/(dashboard)/(routes)/recommendation/_components/recommendation-input.tsx
@@ -11,6 +11,8 @@ import toast from "react-hot-toast";
 import { Combobox } from "@/components/ui/combobox";
 import { useState } from "react";
 import Link from "next/link";
+import Image from 'next/image';
+import logo from "@/public/images/logo.png"
 
 interface RecommendationInputProps {
   options: {
@@ -68,8 +70,13 @@ export const RecommendationInput = ({
 
   return (
     <div className="w-full flex flex-col items-center justify-center p-6 gap-5">
-      <div>
-        Heading
+      <div className="flex flex-col text-center">
+        <p className="text-xl font-bold ">
+          Internify AI Job Recommendation
+        </p>
+        <p className="text-sm text-muted-foreground">
+          Write down your requirement and job category to find the most suitable job for you.
+        </p>
       </div>
       <div className="w-2/3">
         <Form {...form}>
@@ -110,19 +117,31 @@ export const RecommendationInput = ({
             <Button className="col-span-12 lg:col-span-2 w-full transition bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:bg-gradient-to-r hover:from-indigo-400 hover:from-10% hover:via-sky-400 hover:via-30% hover:to-emerald-400 hover:to-90%" type="submit" disabled={!isValid || isSubmitting} size="icon">
               Generate
             </Button>
+            <p className="text-xs text-muted-foreground italic text-center">
+              The result might not be 100% accuracy. Use at your own risks.
+            </p>
           </form>
         </Form>
       </div>
       <div>
-        {isSubmitting && (
-          <div>
-            Loading...
+        {isSubmitting && <>
+          <div className='h-full flex flex-col gap-y-4 items-center justify-center mt-5'>
+            <div className='w-10 h-10 relative animate-spin'>
+              <Image
+                alt='logo'
+                fill
+                src={logo}
+              />
+            </div>
+            <p className='text-sm text-muted-foreground'>
+                Internify AI is thinking...
+            </p>
           </div>
-        )}
+        </>}
       </div>
 
       {/* No Result */}
-      {isShowing && responseData && responseData.id ==='' && (
+      {!isSubmitting && isShowing && responseData && responseData.id ==='' && (
         <div className="rounded-xl p-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
           <div className="rounded-lg bg-white p-6 flex flex-col gap-2 text-center">
             <p>
@@ -136,7 +155,7 @@ export const RecommendationInput = ({
       )}
 
       {/* Result */}
-      {isShowing && responseData && responseData.id !=="" && (
+      {!isSubmitting && isShowing && responseData && responseData.id !=="" && (
         <div className="rounded-xl p-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% w-4/5">
           <div className="rounded-lg bg-white p-6 flex flex-col gap-2 text-center">
             <div className="flex flex-col">
