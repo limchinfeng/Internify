b3f2e4fbc54fea3a6145c189dda4ae13f459979a
Perming1126 <perming@student.usm.my>
2024-01-27 22:49:42 +0800

    enhance landing page

--- app/(dashboard)/(routes)/(root)/_components/user-home.tsx
+++ app/(dashboard)/(routes)/(root)/_components/user-home.tsx
@@ -9,6 +9,17 @@ import avatar from "@/public/images/placeholder.jpg"
 import { User2 } from "lucide-react";
 import { GraduationCap, ScrollText, Rocket, Heart } from "lucide-react";
 import { useRouter } from "next/navigation";
+import TypewriteroComponent from "typewriter-effect"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
 
 export const UserHome = ({
   currentUser
@@ -45,8 +56,60 @@ export const UserHome = ({
     router.push('/favorite');
   }
 
+  const testimonials = [
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+    {
+      name: "Supershuaifeng",
+      avatar: "F",
+      title: "Software Engineer",
+      description: "Best application ever!"
+    },
+
+  ]
+
   return (
-    <div className="w-full items-center justify-center flex flex-col p-2">
+    <div className="w-full items-center justify-center flex flex-col ">
       <div className="border-4 shadow-md flex md:flex-row flex-col items-center justify-center w-2/3 mt-10 py-8 gap-4">
         <div>
           <Image
@@ -61,12 +124,12 @@ export const UserHome = ({
           <p >{currentUser.email}</p>
         </div>
         <div>
-          <Button 
-            className="lg:w-48 lg:h-12 w-32 h-9 p-5" 
-            onClick={handleClickProfile} 
+          <Button
+            className="lg:w-48 lg:h-12 w-32 h-9 p-5"
+            onClick={handleClickProfile}
             disabled={buttonClickedProfile}
           >
-            <User2 className="mr-2 h-4 w-4" /> 
+            <User2 className="mr-2 h-4 w-4" />
             Profile
           </Button>
         </div>
@@ -74,26 +137,51 @@ export const UserHome = ({
 
 
       <div className="flex flex-col justify-center items-center my-10 mx-auto">
-        <h2>Home Page </h2>
+        <h2>Internify: Your Gateway to  </h2>
+        <div className="flex-row flex">
+          <div className='text-transparent bg-clip-text bg-gradient-to-r pb-4
+                from-purple-400 to-pink-600
+            '>
+            <h2>
+              <TypewriteroComponent
+                options={{
+                  strings: [
+                    "Discover",
+                    "Connect",
+                    "Land",
+                    "Succeed",
+                  ],
+                  autoStart: true,
+                  loop: true
+                }}
+
+              />
+            </h2>
+          </div>
+          <div>
+            <h2>Internship</h2>
+          </div>
+        </div>
+
         <p>Where Internships and Simplicity at a place</p>
 
-        <div className="mt-10 grid sm:grid-cols-2 gap-8 grid-cols-1">
+        <div className="my-10 grid sm:grid-cols-2 gap-8 grid-cols-1">
           <div>
-            <Button 
-              className={buttonSize} 
-              variant="outline" 
-              onClick={handleClickListing} 
+            <Button
+              className={buttonSize}
+              variant="outline"
+              onClick={handleClickListing}
               disabled={buttonClicked}
             >
-              <GraduationCap className={iconStyle} /> 
+              <GraduationCap className={iconStyle} />
               Internship Listing
             </Button>
           </div>
           <div>
-            <Button 
-              className={buttonSize} 
-              variant="outline" 
-              onClick={handleClickProject} 
+            <Button
+              className={buttonSize}
+              variant="outline"
+              onClick={handleClickProject}
               disabled={buttonClicked}
             >
               <Rocket className={iconStyle} />
@@ -101,10 +189,10 @@ export const UserHome = ({
             </Button>
           </div>
           <div>
-            <Button 
-              className={buttonSize} 
-              variant="outline" 
-              onClick={handleClickResume} 
+            <Button
+              className={buttonSize}
+              variant="outline"
+              onClick={handleClickResume}
               disabled={buttonClicked}
             >
               <ScrollText className={iconStyle} />
@@ -112,10 +200,10 @@ export const UserHome = ({
             </Button>
           </div>
           <div>
-            <Button 
-              className={buttonSize} 
-              variant="outline" 
-              onClick={handleClickFavorite} 
+            <Button
+              className={buttonSize}
+              variant="outline"
+              onClick={handleClickFavorite}
               disabled={buttonClicked}
             >
               <Heart className={iconStyle} />
@@ -123,6 +211,30 @@ export const UserHome = ({
             </Button>
           </div>
         </div>
+
+        <h2>Testimonials</h2>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
+            lg:grid-cols-4 gap-8 mt-10
+        '>
+          {testimonials.map((item) => (
+            <Card key={item.description}
+              className='bg-[#788DAA] border-none text-b'
+            >
+              <CardHeader>
+                <CardTitle className='flex items-center gap-x-2'>
+                  <div>
+                    <h2 className='text-lg'>{item.name}</h2>
+                    <p className='text-zinc-800 text-sm'>{item.title}</p>
+                  </div>
+                </CardTitle>
+              </CardHeader>
+              <CardContent className='pt-4'>
+                {item.description}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+
       </div>
     </div>
   )
