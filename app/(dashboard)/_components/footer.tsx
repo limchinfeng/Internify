4489875732bf6b83d2856c3161a105841996ae2a
Perming1126 <perming@student.usm.my>
2024-01-27 23:08:24 +0800

    add footer

---
+++ app/(dashboard)/_components/footer.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import React from "react";
+import styles from "./../../../styles/Footer.module.scss";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import Link from "next/link";
+import { SiDiscord } from "react-icons/si";
+import Image from 'next/image'
+
+
+const Footer = () => {
+
+    return (
+        <>
+            <div className={styles["footer-container"]}>
+                <div className={styles["content-container"]}>
+                    <div className={styles["about"]}>
+
+                        <a href="/" className={`a ${styles["website-link"]}`} >
+                            https://internify-deploy.vercel.app/
+                        </a>
+                        <div className={styles["about-text"]}>
+                            <p>Internify is a comprehensive internship portal designed to connect students with their next professional stepping stone. By participating in Internify, students can enhance their industry acumen, engage with potential employers, and foster the skills essential for their future careers    </p>
+                        </div>
+                        <div className={styles["copyright"]}>
+                            <p>©2024 Internify. All rights reserved.</p>
+                        </div>
+                        <div className="py-2">
+                            <Image
+                                src="/Internify-logo.png"
+                                width={80}
+                                height={50}
+                                alt="logo"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles["physical"]}>
+                        <div className={styles["title"]}>
+                            <h2>Students</h2>
+                        </div>
+                        <div className={styles["column"]}>
+                            <Link href="/profile" ><p>Profile</p></Link>
+                            <Link href="/listing"><p>Intern Listing</p></Link>
+                            <Link href="/recommendation"><p>Recommendation</p></Link>
+
+                        </div>
+                    </div>
+                    <div className={styles["online"]}>
+                        <div className={styles["title"]}>
+                            <h2>Company</h2>
+                        </div>
+                        <div className={styles["column"]}>
+                            <Link href="/profile"><p>Profile</p></Link>
+                            <Link href="/project"><p>Project Showcase</p></Link>
+                        </div>
+                    </div>
+
+                    <div className={styles["footer-bottom"]}>
+
+                        <div className={styles["social-media"]}>
+                            <a
+                                target="_blank"
+                                href="https://www.instagram.com/odyssey_hackfest/" rel="noreferrer"
+                                className="a"
+                            >
+                                <AiFillInstagram className={styles["icons"]} />
+                            </a>
+                            <a
+                                target="_blank"
+                                href="https://www.facebook.com/Odyssey-Hackfest-2022-110097031700097" rel="noreferrer"
+                            >
+                                <AiFillFacebook className={styles["icons"]} />
+                            </a>
+                            <a
+                                target="_blank"
+                                href="https://www.linkedin.com/company/odyssey-hackfest-2022/" rel="noreferrer"
+                            >
+                                <AiFillLinkedin className={styles["icons"]} />
+                            </a>
+                            <a
+                                target="_blank"
+                                href="https://www.youtube.com/channel/UC-D91b8qZv_-xZan_p_CzzQ/featured" rel="noreferrer"
+                            >
+                                <AiFillYoutube className={styles["icons"]} />
+                            </a>
+                            <a
+                                target="_blank"
+                                href="https://discord.gg/M9V2SFmwW8" rel="noreferrer"
+                            >
+                                <SiDiscord className={styles["icons"]} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
